036b824da3ceeb95acba3c1b2defa4f084ec20cd
ShadowFoxRonin <rezcicovalexandr@gmail.com>
2025-03-16 23:29:08 +0200

    1. Two Sum

---
+++ arrays_and_hasing/two_sum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def two_sum_two_for(arr: List[int], target: int) -> List[int]:
+    result = []
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i != j and arr[i] + arr[j] == target:
+                result.append(i)
+                result.append(j)
+                return result
+    return result
+
+
+print(two_sum_two_for([2, 7, 11, 15], 9))  # [0,1]
+
+
+def two_sum(arr: List[int], target: int) -> List[int]:
+    collection_of_numbers = {}
+    for i in range(len(arr)):
+        search_value = target - arr[i]
+        if collection_of_numbers.__contains__(search_value):
+            return [collection_of_numbers[search_value], i]
+        elif not collection_of_numbers.__contains__(arr[i]):
+            collection_of_numbers[arr[i]] = i
+
+    return []
+
+
+print(two_sum([2, 7, 11, 15], 9))  # [0,1]
